4518e4433c3cb4d8a525e067c4fb81668adb729d
yamanteo <yamanteoman@hotmail.com>
2026-05-28 04:44:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from dotenv import load_dotenv
+app = FastAPI()
 
 load_dotenv()
 
